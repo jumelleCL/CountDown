05aa0f3bbcf0a7cc754195548b066c966d32d432
camilo <jumellecamilo@gmail.com>
2023-11-10 23:04:59 +0100

    Added function election of holidays

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const newYear = '2024/01/01';
+let daySelected = '2024/03/21';
 const other = document.getElementById('opt5');
 const dayEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
@@ -11,40 +11,24 @@ const otherday = document.getElementById('other-select');
 otherday.style.display = 'none';
 back.style.display = 'none';
 
-select.addEventListener('click', ()=> {
-    select.addEventListener('change', ()=> {
-        if (select.value === "Other") 
-        {
-            select.style.display = 'none';
-            otherday.style.display = 'flex';
-        }else{
-            switch (select.value) {
-                case value:
-                    
-                    break;
-            
-                default:
-                    break;
-            }
-        }
-    })
-})
-
-back.addEventListener('click', ()=>{
-    select.style.display = 'flex';
-    otherday.style.display = 'none';  
-})
-
 
 function countDown(){
-    const newYearDate = new Date(newYear);
+    const newYearDate = new Date(daySelected);
     const currentDate = new Date();
 
-    const totalSeconds = (newYearDate - currentDate)/ 1000;
+    // Configurar la fecha objetivo sin el año
+    newYearDate.setFullYear(currentDate.getFullYear());
+
+    // Verificar si la fecha objetivo ya ha pasado en el año actual
+    if (currentDate > newYearDate) {
+        newYearDate.setFullYear(currentDate.getFullYear() + 1);
+    }
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds/ 3600) %24;
-    const min = Math.floor(totalSeconds/60) % 60;
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const min = Math.floor(totalSeconds / 60) % 60;
     const secs = Math.floor(totalSeconds) % 60;
 
     dayEl.innerHTML = days;
@@ -55,6 +39,39 @@ function countDown(){
 function formatTime(time) {
     return time < 10 ? (`0${time}`) : time;
 }
-countDown();
+function updateCountdown() {
+    countDown();
+}
 
-setInterval(countDown, 1000);
+select.addEventListener('change', () => {
+    switch (select.value) {
+        case 'Halloween':
+            daySelected = '2024/10/31';
+            break;
+        case 'Christmas':
+            daySelected = '2024/12/25';
+            break;
+        case 'Easter':
+            daySelected = '2024/03/31';
+            break;
+        case 'New Year':
+            daySelected = '2024/01/01';
+            break;
+        case 'Other':
+            select.style.display = 'none';
+            otherday.style.display = 'flex';
+            return; // Exit the function to avoid calling countDown(daySelected)
+        default:
+            break;
+    }
+
+    updateCountdown();
+});
+
+back.addEventListener('click', ()=>{
+    select.style.display = 'flex';
+    otherday.style.display = 'none';  
+})
+
+updateCountdown();
+setInterval(() => updateCountdown(), 1000);
